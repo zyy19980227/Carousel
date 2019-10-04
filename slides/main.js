34005b43646d7340ee0121ee32ff00076b1bf46b
Jackson <1090359802@qq.com>
2019-10-04 23:26:27 +0800

    重做修复bug

--- slides/main.js
+++ slides/main.js
@@ -1,41 +1,76 @@
-let n=0
-start()
-setInterval(()=>{
-    makeLeave(getImage(n))
-    .one('transitionend',(e)=>{
-        makeEnter($(e.currentTarget))
+let $buttons=$('#buttonWrapper>button')
+let $slides=$('#slides')
+let $images=$slides.children('img')
+let current=0
+makeFakeSlides()
+$slides.css({transform:'translateX(-300px)'})
+bindEvents()
+//鼠标点击切换
+$(next).on('click',function(){
+    goToSlide(current+1)
+})
+$(previous).on('click',function(){
+    goToSlide(current-1)
+})
+//每两秒滚动一次
+let timer=setInterval(function(){
+    goToSlide(current+1)
+},2000)
+//鼠标悬停停止滚动
+$('.container').on('mouseenter',function(){
+    window.clearInterval(timer)
+})
+//鼠标离开开始滚动
+$('.container').on('mouseleave',function(){
+    timer=setInterval(function(){
+        goToSlide(current+1)
+    },2000)
+})
+
+
+
+
+
+
+function makeFakeSlides(){   //制造头尾虚假页面
+    let $firstCopy=$images.eq(0).clone(true)
+    let $lastCopy=$images.eq($images.length-1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
+function bindEvents(){     
+    $('#buttonWrapper').on('click','button',function(e){
+        let $button=$(e.currentTarget)
+        let index=$button.index()
+        goToSlide(index)
     })
-    makeCurrent(getImage(n+1))
-    n+=1
-},3000)
-
-
-
-//封装函数
-function getImage(n){
-    return $(`.images>img:nth-child(${x(n)})`)
 }
-function x(n){
-    if(n>3){
-        n=n%3
-        if(n===0){
-            n=3
-        }
+function goToSlide(index){  //无缝轮播函数
+    if(index>$buttons.length-1){
+        index=0
+    }else if(index<0){
+        index=$buttons.length-1
     }
-    return n
-}
-function start(){  //初始化函数
-    n=1
-    $('.images>img:nth-child(1)').addClass('current')
-    .siblings().addClass('enter')
-}
-function makeCurrent($node){
-    $node.removeClass('enter leave').addClass('current')
-}
-function makeLeave($node){
-    $node.removeClass('enter current').addClass('leave')
-    return $node
-}
-function makeEnter($node){
-    $node.removeClass('current leave').addClass('enter')
+    if(current===$buttons.length-1&&index===0){
+            //last to first
+            $slides.css({transform:`translateX(${-($buttons.length+1)*300}px)`})
+            .one('transitionend',function(){
+                $slides.hide()
+                    .offset()
+                $slides.css({transform:`translateX(${-(index+1)*300}px)`})
+                    .show()
+            })
+        }else if(current===0&&index===$buttons.length-1){
+            //first to last
+            $slides.css({transform:`translateX(0px)`})
+            .one('transitionend',function(){
+                $slides.hide()
+                    .offset()
+                $slides.css({transform:`translateX(${-(index+1)*300}px)`})
+                    .show()
+            })
+        }else{
+            $slides.css({transform:`translateX(${-(index+1)*300}px)`})
+        }
+        current=index
 }
